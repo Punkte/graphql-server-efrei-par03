f98be6afd2a92b083b89c44189626963cb56cf29
Teepan <paartheepan94@gmail.com>
2025-02-24 17:24:42 +0100

    feat: add exercise session

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,17 +1,37 @@
+import { getClosestColor } from "./utils/colors.js";
+
 const doctorsData = [
   {
     name: 'Samia Mekame',
-    speciality: 'OPHTALMOLOGIST',
+    speciality: 'OPHTALMOLOGIST' as const,
   },
   {
     name: 'Catherine Bedoy',
-    speciality: 'PSYCHOLOGIST',
+    speciality: 'PSYCHOLOGIST' as const,
   },
 ];
 
 
+type Speciality = 'PSYCHOLOGIST' | 'OPHTALMOLOGIST'
+
 export const resolvers = {
   Query: {
-    doctors: () => doctorsData
+    doctors: (_: unknown, {specialities}: {specialities?: Speciality[]}) => specialities ? doctorsData.filter(doctor => specialities.includes(doctor.speciality)) : doctorsData,
+    add: (_: unknown, {number1, number2}: {number1: number, number2: number}) => number1 + number2,
+    substract: (_: unknown, {number1, number2}: {number1: number, number2: number}) => number1 - number2,
+    multiply: (_: unknown, {number1, number2}: {number1: number, number2: number}) => number1 * number2,
+    divide: (_: unknown, {number1, number2}: {number1: number, number2: number}) => {
+      if (number2 === 0) {
+        throw new Error('cannot divide by 0')
+      }
+
+      return number1 / number2
+    },
+    closestColor: (_: unknown, {hexa}: {hexa: string}) => {
+      if (!(hexa.match(/^#[0-9a-fA-F]{6}/))) {
+        throw new Error('Provided hexa is not valid')
+      }
+      return getClosestColor(hexa, ["#FF5733", "#33FF57", "#3357FF"]);
+    }
   }
 }
--- src/schema.ts
+++ src/schema.ts
@@ -2,7 +2,12 @@ import gql from "graphql-tag";
 
 export const typeDefs = gql`
   type Query {
-    doctors: [Doctor]
+    doctors(specialities: [Speciality!]): [Doctor]
+    add(number1: Float!, number2: Float!): Float!
+    substract(number1: Float!, number2: Float!): Float!
+    multiply(number1: Float!, number2: Float!): Float!
+    divide(number1: Float!, number2: Float!): Float
+    closestColor(hexa: String!): String
   }
 
   type Doctor {
